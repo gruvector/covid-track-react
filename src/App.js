3f542612bb52776bccb1be29559475e386c0b6fc
gruvector <148240965+gruvector@users.noreply.github.com>
2024-02-09 22:21:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
 import image from './images/image.png';
-
 class App extends React.Component {
   state = {
     data: {},
